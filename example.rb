14e67348d7c4c86cf1164828b91aa21b1d052f5a
Kensuke Numakura <knmkr3gma@gmail.com>
2017-10-16 14:21:02 +0900

    refactor

--- example.rb
+++ example.rb
@@ -6,19 +6,17 @@ require 'httpclient'  # gem install httpclient
 
 client = HTTPClient.new
 token = 'GENOMELINKTEST'
-headers = {
-  Authorization: "Bearer #{token}"
-}
+headers = { Authorization: "Bearer #{token}" }
 
 phenotype = 'eye-color'
 population = 'european'
 report_url = "https://genomicexplorer.io/v1/reports/#{phenotype}/?population=#{population}"
-response = client.get report_url, {}, headers
-puts response.body
+response = client.get(report_url, header: headers)
+puts JSON.parse(response.body)
 
 chrom = 'chr1'
 start_pos = '100000'
 end_pos = '100500'
 sequence_url = "https://genomicexplorer.io/v1/genomes/sequence/?region=#{chrom}:#{start_pos}-#{end_pos}"
-response = client.get sequence_url, {}, headers
-puts response.body
+response = client.get(sequence_url, header: headers)
+puts JSON.parse(response.body)
